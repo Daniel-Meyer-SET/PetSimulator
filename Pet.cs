11a5dc1908eb5cd03e35adf25eedc018019d9b1c
Daniel Meyer <dmeyer3583@conestogac.on.ca>
2024-01-28 14:34:14 -0500

    added Pet class

---
+++ Pet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetSimulator
+{
+    public class Pet
+    {
+        int health = 10;
+        int hunger = 10;
+        int happiness = 10;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Pet Simulator!");
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Pet Simulator!");
+
+        
+    }
+}
