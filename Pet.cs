325516227b228bf4d3c71a4c7d5f55f35c84f215
Daniel Meyer <dmeyer3583@conestogac.on.ca>
2024-01-30 16:14:44 -0500

    Added Pet Class Constructor

--- Pet.cs
+++ Pet.cs
@@ -7,21 +7,34 @@ using System.Threading.Tasks;
 namespace PetSimulator
 {
     public class Pet
-    {
-        int health = 10;
-        int hunger = 10;
-        int happiness = 10;
+    {  //
+       private int health { get ; set; }
+       private  int hunger { get; set; }
+       private int happiness { get; set; }
+       private string name { get; set; }
+       private string type { get; set; }
 
+        public Pet(string initType, string initName) {
+            health = 10;
+            hunger = 0;
+            happiness =10;
+            name = initName;
+            type = initType;
+        }
         public void feed() {
-
+            hunger = hunger - 4;
+            health = health +1;
         }
 
         public void play() {
-
+            happiness = happiness +4;
+            hunger = hunger + 2;
         }
 
-        public void rest() { 
-        
+        public void rest() {
+            health = health + 4;
         }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Pet Simulator!");
-
-        
+        Console.WriteLine("Welcome to Pet Simulator!\n");
+        Console.WriteLine("\nChoose a type of Pet ");
+        Console.WriteLine();
     }
 }
