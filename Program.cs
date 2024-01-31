ef84ae4ec41899526629adbedaca4f61f23502ec
Daniel Meyer <dmeyer3583@conestogac.on.ca>
2024-01-30 23:09:07 -0500

    Added Pet Selection Menu

--- Program.cs
+++ Program.cs
@@ -4,6 +4,41 @@
     {
         Console.WriteLine("Welcome to Pet Simulator!\n");
         Console.WriteLine("\nChoose a type of Pet ");
-        Console.WriteLine();
+        Console.WriteLine(" 1) Dog\n 2) Cat\n \n  3) Rabbit");
+
+      
+
+        string petType = " ";
+        bool typeSelected = false;
+        while (typeSelected == false)
+        {
+            switch (Console.ReadKey().KeyChar) { 
+            case '1':
+                    petType = "Dog";
+                    Console.WriteLine("\nYour new pet will be a Dog!");
+                    typeSelected = true; break;
+                    
+            
+            case '2':
+                    petType = "Cat";
+                    Console.WriteLine("\nYour new pet will be a Cat!");
+                    typeSelected = true; break;
+                    
+            
+            case '3':
+                    petType = "Rabbit";
+                    Console.WriteLine("\nYour new pet will be a Rabbit!");
+                    typeSelected = true; break;
+                   
+
+            default:
+            Console.WriteLine("Invalid Selection");
+            break;
+
+            
+            }
+
+           
+        }
     }
 }
