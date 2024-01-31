a57b9e8530dc749e58d4a3de6cb64b3faed62701
Daniel Meyer <dmeyer3583@conestogac.on.ca>
2024-01-30 23:13:25 -0500

    Added Line to clear console after input keystrokes to keep UI clean

--- Program.cs
+++ Program.cs
@@ -3,36 +3,44 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Pet Simulator!\n");
-        Console.WriteLine("\nChoose a type of Pet ");
-        Console.WriteLine(" 1) Dog\n 2) Cat\n \n  3) Rabbit");
-
+       
       
 
         string petType = " ";
         bool typeSelected = false;
         while (typeSelected == false)
         {
+            Console.WriteLine("\nChoose a type of Pet ");
+            Console.WriteLine(" 1) Dog\n 2) Cat\n \n  3) Rabbit");
+
             switch (Console.ReadKey().KeyChar) { 
             case '1':
+                    Console.Clear();
                     petType = "Dog";
                     Console.WriteLine("\nYour new pet will be a Dog!");
+                    
                     typeSelected = true; break;
                     
             
             case '2':
                     petType = "Cat";
+                    Console.Clear();
                     Console.WriteLine("\nYour new pet will be a Cat!");
+                   
                     typeSelected = true; break;
                     
             
             case '3':
                     petType = "Rabbit";
+                    Console.Clear();
                     Console.WriteLine("\nYour new pet will be a Rabbit!");
+                   
                     typeSelected = true; break;
                    
 
             default:
-            Console.WriteLine("Invalid Selection");
+                    Console.Clear();
+                    Console.WriteLine("Invalid Selection");
             break;
 
             
